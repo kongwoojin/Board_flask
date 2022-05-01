4460bca67f90fe27ab63ebe271b8cb6ac8c746b5
kongwoojin <kongwoojin03@gmail.com>
2022-05-01 22:40:07 +0900

    Add labels

--- forms.py
+++ forms.py
@@ -4,13 +4,13 @@ from wtforms.validators import DataRequired, EqualTo
 
 
 class SignUpForm(FlaskForm):
-    userid = StringField('userid', validators=[DataRequired()])
-    username = StringField('username', validators=[DataRequired()])
-    email = EmailField('email', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired(), EqualTo('re_password')])
-    re_password = PasswordField('re_password', validators=[DataRequired()])
+    userid = StringField('User ID', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('re_password')])
+    re_password = PasswordField('Password confirm', validators=[DataRequired()])
 
 
 class SignInForm(FlaskForm):
-    userid = StringField('userid', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
+    userid = StringField('User ID', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
